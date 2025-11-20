5025c411fa1e526706178bcd561cf26ac09e59e0
Sivert Lunde <sivert.lunde@gmail.com>
2025-11-20 14:08:41 +0100

    log user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const getUser = async () => {
     const response = await fetch(`/api/firebase/user/${discordUser?.id ?? ""}`)
-    console.log(response)
+    console.log(await response.json())
   }
 
   if (ready) getUser()
