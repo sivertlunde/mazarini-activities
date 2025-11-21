2f9431b4bace9246f62e0b9f5efda2113f02b57c
Sivert Lunde <sivert.lunde@gmail.com>
2025-11-21 14:38:02 +0100

    update logging

--- src/app/api/message/reward/route.ts
+++ src/app/api/message/reward/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
   console.log("Server fikk reward:", reward)
   const rewardHelper = new RewardHelper()
   const messageBody = await rewardHelper.resolveReward(userId, reward)
-  console.log("Prøver å sende melding:", messageBody)
+  console.log("Prøver å sende melding:", JSON.stringify(messageBody, null, 2))
   // Now you can safely post to Discord using your bot token
   const restResp = await fetch(
     `https://discord.com/api/channels/${channelId}/messages`,
